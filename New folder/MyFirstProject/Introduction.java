c4b56f5e1b3003e2dbabdf02a7ab5f2eab728ce2
Eric <44299435+ecalderon8@users.noreply.github.com>
2018-10-22 02:15:41 -0400

    Add files via upload
    Problem 2 and 3 for Assignment 1

---
+++ New folder/MyFirstProject/Introduction.java
@@ -0,0 +1,8 @@
+/**This Introduction belongs to Eric Calderon*/
+
+public class Introduction {
+	public static void main(String[] args) {
+		System.out.println ("myIntroduction");
+	
+	}
+}
---
+++ New folder/MyName/MyName.java
@@ -0,0 +1,11 @@
+
+public class MyName {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, My Name Is <Eric Calderon>");
+		System.out.println("My favorite color is <Blue>");
+		System.out.println("If I won the lottery, I would <Completely pay off my school loans>");
+			
+	}
+
+}
